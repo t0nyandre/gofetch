9da45a95b49155ebf38b25dbdfbfc082d36b4502
t0nyandre <post@tonyandre.co>
2022-11-10 21:08:17 +0100

    Added function to extract username and hostname

---
+++ cmd/info.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"os"
+	"os/user"
+)
+
+// Username returns the username of the logged in user in string format
+func Username() string {
+	current_user, err := user.Current()
+	username := "Unknown"
+	if err == nil {
+		username = current_user.Username
+	}
+	return username
+}
+
+// Hostname returns the hostname of the system
+func Hostname() string {
+	hostname, err := os.Hostname()
+	host := "Unknown"
+	if err == nil {
+		host = hostname
+	}
+	return host
+}
